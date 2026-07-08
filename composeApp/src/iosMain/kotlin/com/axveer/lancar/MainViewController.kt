93b89b4dc0155e0c84b56b2a19abc0c52132ddea
Vladimir Babin <vovababin@gmail.com>
2026-07-08 14:59:14 +0800

    feat: iOS host wiring AppModule

--- composeApp/src/iosMain/kotlin/com/axveer/lancar/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/axveer/lancar/MainViewController.kt
@@ -1,5 +1,20 @@
 package com.axveer.lancar
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.axveer.lancar.data.ContentRepository
+import com.axveer.lancar.data.DriverFactory
+import com.axveer.lancar.data.ProgressRepository
+import com.axveer.lancar.db.LancarDatabase
+import com.axveer.lancar.platform.IosAudioPlayer
+import com.axveer.lancar.ui.App
+import com.axveer.lancar.ui.AppModule
 
-fun MainViewController() = ComposeUIViewController { App() }
+fun MainViewController() = ComposeUIViewController {
+    val db = LancarDatabase(DriverFactory().createDriver())
+    val appModule = AppModule(
+        content = ContentRepository(),
+        progress = ProgressRepository(db),
+        audio = IosAudioPlayer(),
+    )
+    App(appModule)
+}
